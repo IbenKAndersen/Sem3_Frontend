50833e9ffa9c96914e34b5a1ac16a93a04cee057
Iben A <cph-ia62@cphbusiness.dk>
2019-11-25 13:09:09 +0100

    changes to plugin

---
+++ build/precache-manifest.50231df0c3bd2c00e06e73d7fa6d38dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f82594ee4ea03dc3fa82a5671f8984cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e257d98d55d128a0ff13",
+    "url": "/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "5c1b837a4c6dbb7e08a2",
+    "url": "/static/css/main.17ed3c4c.chunk.css"
+  },
+  {
+    "revision": "e257d98d55d128a0ff13",
+    "url": "/static/js/2.cadf9a39.chunk.js"
+  },
+  {
+    "revision": "5c1b837a4c6dbb7e08a2",
+    "url": "/static/js/main.bc934d80.chunk.js"
+  },
+  {
+    "revision": "38736351c0e9ea20c9f8",
+    "url": "/static/js/runtime-main.70d09be9.js"
+  }
+]);
