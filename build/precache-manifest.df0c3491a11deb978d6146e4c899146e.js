cfa99fde86ba156f08ecf260403eaafde1117070
TheDanishWonder <44898418+TheDanishWonder@users.noreply.github.com>
2019-12-05 14:16:33 +0100

    location marks updated

---
+++ build/precache-manifest.df0c3491a11deb978d6146e4c899146e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "134d42c5ac434a86004531ffc4298511",
+    "url": "/index.html"
+  },
+  {
+    "revision": "165808346e1632624491",
+    "url": "/static/css/2.6f9bb5da.chunk.css"
+  },
+  {
+    "revision": "6432c6fba52694aea8ca",
+    "url": "/static/css/main.af825cd1.chunk.css"
+  },
+  {
+    "revision": "165808346e1632624491",
+    "url": "/static/js/2.09fd450b.chunk.js"
+  },
+  {
+    "revision": "6432c6fba52694aea8ca",
+    "url": "/static/js/main.3bcfd9c1.chunk.js"
+  },
+  {
+    "revision": "38736351c0e9ea20c9f8",
+    "url": "/static/js/runtime-main.70d09be9.js"
+  }
+]);
--- src/components/Location.js
+++ src/components/Location.js
@@ -28,22 +28,113 @@ export class Location extends Component {
         style={{
           position: "relative",
           height: "calc(100vh - 20px)"
-        }}>
-        <Map style={{}} google={this.props.google} initialCenter={{ lat: 55.642954, lng: 12.545465 }} zoom={11}>
+        }}
+      >
+        <Map
+          style={{}}
+          google={this.props.google}
+          initialCenter={{ lat: 55.642954, lng: 12.545465 }}
+          zoom={11}
+        >
           <Marker
-            title={'Copenhagen Central Station'}
-            name={'Central Station'}
+            title={"Copenhagen Central Station"}
+            name={"Central Station"}
             position={{ lat: 55.672388, lng: 12.563445 }}
           />
           <Marker
-            title={'Copenhagen Airport'}
-            name={'Airport'}
+            title={"Copenhagen Airport"}
+            name={"Airport"}
             position={{ lat: 55.629953, lng: 12.637484 }}
           />
           <Marker
-            title={'Copenhagen Commercial Neighborhood'}
-            name={'Business Area'}
-            position={{ lat: 55.614174, lng: 12.472494 }}
+            title={"Malmø Midtown"}
+            name={"Malmø Midtown"}
+            position={{ lat: 55.60851, lng: 12.999255 }}
+          />
+          <Marker
+            title={"Kongens Lyngby City Hall"}
+            name={"Kongens Lyngby City Hall"}
+            position={{ lat: 55.7684, lng: 12.50407 }}
+          />
+          <Marker
+            title={"Vanløse Mall"}
+            name={"Vanløse Mall"}
+            position={{ lat: 55.691541, lng: 12.480927 }}
+          />
+          <Marker
+            title={"Frederiksberg Garden"}
+            name={"Frederiksberg Garden"}
+            position={{ lat: 55.675132, lng: 12.519569 }}
+          />
+          <Marker
+            title={"Stockholm Central Station"}
+            name={"Stockholm Central Station"}
+            position={{ lat: 59.331215, lng: 18.058206 }}
+          />
+          <Marker
+            title={"Helsinki Mall"}
+            name={"Helsinki Mall"}
+            position={{ lat: 60.170562, lng: 24.942227 }}
+          />
+          <Marker
+            title={"London Marble Arch"}
+            name={"London Marble Arch"}
+            position={{ lat: 51.513212, lng: -0.160321 }}
+          />
+           <Marker
+            title={"London Westminster"}
+            name={"London Westminster"}
+            position={{ lat: 51.497039, lng: -0.138996 }}
+          />
+          <Marker
+            title={"Dublin Smithfield"}
+            name={"Dublin Smithfield"}
+            position={{ lat: 53.346629, lng: -6.280353 }}
+          />
+          <Marker
+            title={"Billund Airport P1"}
+            name={"Billund Airport P1"}
+            position={{ lat: 55.747767, lng: 9.143861 }}
+          />
+          <Marker
+            title={"Billund Resort LEGOLAND"}
+            name={"Billund Resort LEGOLAND"}
+            position={{ lat: 55.737208, lng: 9.124908 }}
+          />
+          <Marker
+            title={"Bornholm Aakirkeby"}
+            name={"Bornholm Aakirkeby"}
+            position={{ lat: 55.075613, lng: 14.918001 }}
+          />
+          <Marker
+            title={"Odense Midtown"}
+            name={"Odense Midtown"}
+            position={{ lat: 55.393893, lng: 10.383769 }}
+          />
+          <Marker
+            title={"Flensburg Tarup"}
+            name={"Flensburg Tarup"}
+            position={{ lat: 54.782299, lng: 9.470083 }}
+          />
+          <Marker
+            title={"Hamborg Überseeallee"}
+            name={"Hamborg Überseeallee"}
+            position={{ lat: 53.541509, lng: 9.997682 }}
+          />
+          <Marker
+            title={"Hamborg Airport"}
+            name={"Hamborg Airport"}
+            position={{ lat: 53.634785, lng: 10.006212 }}
+          />
+          <Marker
+            title={"Berlin Alexanderplatz"}
+            name={"Berlin Alexanderplatz"}
+            position={{ lat: 52.522959, lng: 13.414774 }}
+          />
+          <Marker
+            title={"Oslo Central Station"}
+            name={"Oslo Central Station"}
+            position={{ lat: 59.910816, lng: 10.749853 }}
           />
           <InfoWindow
             marker={this.state.activeMarker}
